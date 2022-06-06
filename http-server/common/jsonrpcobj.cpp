90debe4d0276074d7986d449832276d3ca1253c0
lzy <luozhiyong>
2022-06-06 13:09:48 +0800

    string

--- http-server/common/jsonrpcobj.cpp
+++ http-server/common/jsonrpcobj.cpp
@@ -2,8 +2,11 @@
 
 bool JsonRpcObj::getString(jsonrpc_request* r, int id, char *s)
 {
-
-
+    char buffer[10] = {0}; static char buf[256] = {0};
+    sprintf(buffer , "$[%d]" , id);
+    int n = mjson_get_string(r->params, r->params_len, buffer, buf , sizeof(buf));
+    if(n > 0) qstrcpy(s, buf); else s[0] = '\0';
+    return n == -1 ?false:true;
 }
 
 double JsonRpcObj::getNumber(jsonrpc_request* r, int id)
@@ -23,3 +26,21 @@ QVector<double> JsonRpcObj::getNumbers(jsonrpc_request* r, int num)
     }
     return res;
 }
+
+/*
+ *
+int mjson_get_number(const char *s, int len, const char *path, double *v) {
+  const char *p;
+  int tok, n;
+  if ((tok = mjson_find(s, len, path, &p, &n)) == MJSON_TOK_NUMBER) {
+    if (v != NULL) *v = mystrtod(p, NULL);
+  }
+  return tok == MJSON_TOK_NUMBER ? 1 : 0;
+}
+int mjson_get_string(const char *s, int len, const char *path, char *to,
+                     int n) {
+  const char *p;
+  int sz;
+  if (mjson_find(s, len, path, &p, &sz) != MJSON_TOK_STRING) return -1;
+  return mjson_unescape(p + 1, sz - 2, to, n);
+}*/
