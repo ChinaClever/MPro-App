43d59d51186346e0ea0577cb40deda8f8e164c92
lzy <luozhiyong>
2022-08-04 17:15:00 +0800

    ａａ

--- cores/apps/app_start.cpp
+++ cores/apps/app_start.cpp
@@ -60,7 +60,7 @@ void App_Start::startThreadSlot()
 
     Data_Core::bulid();
     Mb_Core::bulid(this);
-    OP_Core::bulid(this)->startFun();
+    //OP_Core::bulid(this)->startFun();
     //Cascade_Core::bulid(this)->startFun();
 
 }
--- cores/commons/secret/sercret_core.cpp
+++ cores/commons/secret/sercret_core.cpp
@@ -1,3 +1,8 @@
+/*
+ *
+ *  Created on: 2022年10月1日
+ *      Author: Lzy
+ */
 #include "sercret_core.h"
 
 Sercret_Core::Sercret_Core()
