c1014e120feb52fd15be1711a808645bae7e7987
lzy <luozhiyong>
2022-08-08 10:58:43 +0800

    修改协议

--- http_server/pdurpcobj.cpp
+++ http_server/pdurpcobj.cpp
@@ -48,9 +48,9 @@ char *PduRpcObj::pduReadParam(mg_str &r)
 char *PduRpcObj::pduSetParam(mg_str &r)
 {
     QVector<double> its = mObj->getNumbers(r, 6);
-    QString value = mObj->getString(r, 5);
-    bool ret = mWebIpc->setCfg((uint)its.at(0), (uchar)its.at(1),
-                                  (uchar)its.at(2), value);
+    QVariant value = mObj->getString(r, 5);
+    if(value.toString().isEmpty()) value = mObj->getNumber(r, 5);
+    bool ret = mWebIpc->setCfg((uint)its.at(0), (uchar)its.at(1), (uchar)its.at(2), value);
     return responRpcData(its, ret?1:0);
 }
 
