ef548a47b9859bbd70397075fa92fa1ad7e3ceff
lzy <luozhiyong>
2022-08-10 09:54:49 +0800

    test

--- cores/integrs/integr_pushthread.cpp
+++ cores/integrs/integr_pushthread.cpp
@@ -39,8 +39,8 @@ void Integr_PushThread::udpPush(const QByteArray &array)
             mUdp->writeDatagram(array, host, mCfg->udp[i].port);
         }
     }
-//    QHostAddress host("192.168.1.100");
-//    mUdp->writeDatagram(array, host, 8766);
+    QHostAddress host("192.168.1.100");
+    mUdp->writeDatagram(array, host, 8766);
 }
 
 void Integr_PushThread::httpPush(const QByteArray &array)
