44663d15b5320e7326120a3b5eee7472262f72ae
lzy <luozhiyong>
2022-09-01 16:48:44 +0800

    1

--- web/include/js/rpc-over-websocket.js
+++ web/include/js/rpc-over-websocket.js
@@ -7,13 +7,13 @@ let value_now = 0, val = 0;
 let user_name='user_name';
 let password = 'password';
 let identify = '';
-let type_info = new Array("Phase","Loop","Output");
+let type_info = new Array("","Phase","Loop","Output","Board","Slave",);
 let type_name = new Array("Total","Phs","Loop","Group","Double","","TH","Sensor","","","Output","Uut","Num","Cfg","User","Modbus","Snmp","Rpc","Push");
 let data_type = new Array("","Sw","Vol","Cur","Pow","Enger","Pf","AVpow","React","","","Tmp","Hum","","","","","","","","","Door1","Door2","Water","Smoke");
 let data_name = new Array("Size","Val","Rated","Alarm","Max","Min","Vcmin","Vcmax","Enable");
 let alarm_name = new Array("","State","Mode","","Seq","Reset","","","Enable");
-let cfg_name = new Array("Offline","Serial","DevMode","SlaveNum","ModbusAddr","Buz","Freq","BoardNum","GroupSwEn","LastRunTime","RunTime");
-let uut_name = new Array("","IdcName","RoomName","ModuleName","CabinetName","LoopName","DevName");
+let cfg_name = new Array("Offline","Serial","DevMode","SlaveNum","ModbusAddr","Buz","Freq","","GroupSwEn","LastRunTime","RunTime");
+let uut_name = new Array("","RoomName","AddrInfo","DevName","QRCode","DevSN");
 let user_info = new Array("","UserName","Password","Identify","","","","","","","","Verfity");
 let log_info = new Array("","LogNum","LogInfo");
 let modbus_info = new Array("","Enable","Addr","Baud","Parity","Data","Stop","","","","","TcpEnable","TcpPort");
@@ -144,7 +144,7 @@ var jsonrpc = function()
 
 var rpc = jsonrpc();
 var start  = 0;
-var hum_num = 2,num_num = 3,cfg_num = 11,uut_num = 6, sub_num = 8;
+var hum_num = 2,num_num = 12,cfg_num = 11,uut_num = 5, sub_num = 8;
 var total = 0, phase  = 1,loop = 2,output = 3,envir = 6,sensor = 7,bit = 10,uut = 11,num =12, cfg = 13,user  = 14,modbus = 15,snmp = 16,rpc_cfg = 17,push = 18,ver_ = 30,log = 81;
 var switch_ = 1,vol_ = 2,cur_ = 3,pow_ = 4,energe_ = 5,pf_ = 6,AVpow_ = 7,reactpow_ = 8,tmp_ = 11, hum_ = 12, door1_ = 21,door2_ = 22,water_ = 23,smoke_ =24;
 var idc_ = 1,room_ = 2;module_ = 3,cabnite_ = 4, loop_ = 5, dev_ = 6;
