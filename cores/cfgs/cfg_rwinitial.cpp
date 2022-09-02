bafcb1909df4d7e657db47ae2d82b29c7829271b
lzy <luozhiyong>
2022-09-02 09:04:28 +0800

    增加双电源报警范围

--- cores/cfgs/cfg_rwinitial.cpp
+++ cores/cfgs/cfg_rwinitial.cpp
@@ -97,8 +97,8 @@ void Cfg_RwInitial::initDevData(sDevData *dev)
 {
     initObjData(dev->line, LINE_NUM, 32);
     initObjData(dev->loop, LOOP_NUM, 16);
-    initObjData(dev->dual, OUTPUT_NUM, 10);
-    initObjData(dev->group, GROUP_NUM, 10);
+    initObjData(dev->dual, OUTPUT_NUM, 20);
+    initObjData(dev->group, GROUP_NUM, 14);
     initObjData(dev->output, OUTPUT_NUM, 10);
     initRelayUnit(dev->dual.relay, OUTPUT_NUM);
     initRelayUnit(dev->group.relay, GROUP_NUM);
