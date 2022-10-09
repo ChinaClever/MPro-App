2fcd7db8a88ed0c95ab9e943a97cdb61f754570b
lzy <luozhiyong>
2022-10-09 15:43:14 +0800

    a

--- cores/settings/set_tlscert.h
+++ cores/settings/set_tlscert.h
@@ -2,6 +2,12 @@
 #define SET_TLSCERT_H
 #include "set_output.h"
 
+struct sTlsCertIt
+{
+    sTlsCertIt() {}
+
+};
+
 class Set_TlsCert : public Set_Output
 {
 public:
@@ -9,6 +15,9 @@ public:
 
     QVariant getTlsCert(uchar fc);
     bool setTlsCert(uchar fc, const QVariant &v);
+
+private:
+    sTlsCertIt mTlsCert;
 };
 
 #endif // SET_TLSCERT_H
