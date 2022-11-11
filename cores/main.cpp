2126a80ddec77bb16ab6ad5716938f160e879d6c
lzy <luozhiyong>
2022-11-11 18:06:51 +0800

    a

--- cores/main.cpp
+++ cores/main.cpp
@@ -12,16 +12,5 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     QObject *p = a.parent();
     App_Core::bulid(p);
-
-    qDebug() << "Available drivers:";
-    QStringList drivers = QSqlDatabase::drivers();
-    foreach(QString driver, drivers)
-        qDebug() << " " << driver;
-
-//    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-//    qDebug() << "ODBC driver valid?" << db.isValid();
-
-
-
     return a.exec();
 }
