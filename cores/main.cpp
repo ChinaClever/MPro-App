2ef7e17479d1ff569fe816140c26106154cdd30f
lzy <luozhiyong>
2022-11-14 13:16:29 +0800

    aa

--- cores/main.cpp
+++ cores/main.cpp
@@ -5,12 +5,12 @@
  */
 #include <QCoreApplication>
 #include "app_core.h"
-#include <QSqlDatabase>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QObject *p = a.parent();
     App_Core::bulid(p);
+
     return a.exec();
 }
