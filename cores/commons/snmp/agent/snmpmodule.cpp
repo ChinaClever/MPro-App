72c6e0f93c54d8fa8c47da819eff6755bf3fc577
lzy <luozhiyong>
2022-12-19 10:54:01 +0800

    转码

--- cores/commons/snmp/agent/snmpmodule.cpp
+++ cores/commons/snmp/agent/snmpmodule.cpp
@@ -40,11 +40,11 @@ QSNMPOid SnmpModule::toOid(const QString &oid)
 
 QVariant SnmpModule::snmpGetValue(const QSNMPVar * var)
 {
-    QVariant res;
+    QVariant res; QString str;
     void *v = mShash.value(var->name());
     switch ((QSNMPType_e)var->type()) {
     case QSNMPType_Integer: res = *((int *)v); break;
-    case QSNMPType_OctetStr: res = QString::fromUtf8((char *)v); break;
+    case QSNMPType_OctetStr: res = (char *)v; break;
     default: qDebug() << "Error: snmpGetValue" << var->fullName(); break;
     }
 
