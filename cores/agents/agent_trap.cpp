fe68197989355b4f20c709546b7b1972b2e9d924
lzy <luozhiyong>
2022-12-26 09:33:11 +0800

    aa

--- cores/agents/agent_trap.cpp
+++ cores/agents/agent_trap.cpp
@@ -19,7 +19,7 @@ void Agent_Trap::initTrapSlot()
 {
     mModuleOid = mSnmp->moduleOid();
     Alarm_Updater *alarm = Alarm_Updater::bulid();
-    //connect(alarm, &Alarm_Updater::alarmSig, this, &Agent_Trap::alarmSlot);
+    connect(alarm, &Alarm_Updater::alarmSig, this, &Agent_Trap::alarmSlot);
 
     timer = new QTimer(this);
     //timer->start(3000); ////==========
@@ -51,9 +51,11 @@ void Agent_Trap::alarmSlot(const sDataItem &index, uchar value)
     case DType::Line: oid << 1;  msg += " line "; break;
     case DType::Loop: oid << 2; msg += " loop "; break;
     case DType::Output: oid << 3; msg += " output "; break;
+    case DType::Group: oid << 4; msg += " group "; break;
+    case DType::Dual: oid << 5; msg += " dual "; break;
     case DType::Env: oid << 6; msg += " env "; break;
     case DType::Sensor: oid << 7; msg += " Sensor ";  break;
-    default: qDebug() << Q_FUNC_INFO << index.type; break;
+    default: cout << index.type; break;
     }
     oid << index.id; msg += QString::number(index.id+1);
 
@@ -68,7 +70,7 @@ void Agent_Trap::alarmSlot(const sDataItem &index, uchar value)
     case DTopic::Door2: oid << 2; msg += " Door2 "; break;
     case DTopic::Water: oid << 3; msg += " Water "; break;
     case DTopic::Smoke: oid << 4; msg += " Smoke "; break;
-    default: qDebug() << Q_FUNC_INFO << index.topic; break;
+    default: cout << index.topic; break;
     }
 
     QSNMPOid dstOid = oid;
@@ -79,23 +81,26 @@ void Agent_Trap::alarmSlot(const sDataItem &index, uchar value)
 
     if(value) {
         QString doid = toString(dstOid << 11);
-        //        sAgentCfg *cfg = &Agent_Core::snmpCfg;
+        sAgentCfg *cfg = &Agent_Core::snmpCfg;
+        for(int i=0; i<SNMP_TRAP_SIZE; ++i) {
+            QString ip = cfg->trap[i]; if(ip.size()) {
+                sendTrap(ip, doid, toString(oid), msg);
+                mSnmp->sendTrap(oid);
+            }
+        }
 
-        //        QString ip = cfg->trap1;
-        //        if(ip.size()) sendTrap(ip, doid, toString(oid), msg);
-
-        //        ip = cfg->trap2;
-        //        if(ip.size()) sendTrap(ip, doid, toString(oid), msg);
-        //mSnmp->sendTrap(oid);
-
-        sendTrap("192.168.1.196", doid, toString(oid), msg);
+        //sendTrap("192.168.1.196", doid, toString(oid), msg);
     }
 }
 
-//  命令  版本   -c 共同体  管理机  Enterprise-OID  snmp代理地址   陷阱类型 oid   数据类型    数据类型
+//  V2 命令  版本   -c 共同体  管理机  Enterprise-OID  snmp代理地址   陷阱类型 oid   数据类型    数据类型
 void Agent_Trap::sendTrap(const QString &ip, const QString &dstOid, const QString &oid, const QString &msg)
 {
-    QString cmd = "snmptrap -v 2c -c public %1 '' %2 %3 s '%4'";
-    QString str = cmd.arg(ip, dstOid, oid, msg);
-    system(str.toLatin1().data());
+    if(snmpCfg.enV3) {
+
+    } else {
+        QString cmd = "snmptrap -v 2c -c public %1 '' %2 %3 s '%4'";
+        QString str = cmd.arg(ip, dstOid, oid, msg);
+        system(str.toLatin1().data());
+    }
 }
