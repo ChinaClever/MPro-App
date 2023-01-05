05c4c6894e7256842efdc99ab5b451d97dc364b6
lzy <luozhiyong>
2023-01-05 16:27:15 +0800

    解决级联环境数据问题

--- cores/cascade/cascade_stream.cpp
+++ cores/cascade/cascade_stream.cpp
@@ -37,7 +37,7 @@ QDataStream& operator<<(QDataStream& in, Cascade_Stream& data)
     size = ptr->outputSize; in << size;
     for(int i=0; i<size; ++i) in << toByteArray(ptr->output[i]);
 
-    size = ptr->envSize; in << size;
+    size = ptr->envSize; in << size; size = SENOR_NUM;
     for(int i=0; i<size; ++i) in << toByteArray(ptr->env[i]);
 
     in << toByteArray(ptr->tg);
