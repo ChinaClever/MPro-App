4beddca31f6d0655e5c816c1744aae8143e9d059
lzy <luozhiyong>
2023-01-09 10:58:24 +0800

    线电压处理

---
+++ cores/outputs/op_artu.cpp
@@ -0,0 +1,12 @@
+/*
+ *
+ *  Created on: 2022年10月1日
+ *      Author: Lzy
+ */
+#include "op_artu.h"
+
+OP_ARtu::OP_ARtu(QObject *parent)
+    : QObject{parent}
+{
+
+}
---
+++ cores/outputs/op_artu.h
@@ -0,0 +1,16 @@
+#ifndef OP_ARTU_H
+#define OP_ARTU_H
+
+#include <QObject>
+
+class OP_ARtu : public QObject
+{
+    Q_OBJECT
+public:
+    explicit OP_ARtu(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // OP_ARTU_H
