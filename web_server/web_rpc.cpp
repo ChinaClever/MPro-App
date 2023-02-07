f50c3502318cf2a03a8c1e9ed7baaec04a436e82
lzy <luozhiyong>
2023-02-07 08:54:51 +0800

    登陆修改

--- web_server/web_rpc.cpp
+++ web_server/web_rpc.cpp
@@ -70,7 +70,7 @@ char *Web_Rpc::pduSetParam(mg_str &r)
     QVariant value = mObj->getString(r, 5);
     if(value.toString().isNull()) value = mObj->getNumber(r, 5);
     bool ret = mObj->setCfg(its.at(1), its.at(2), value,its.at(3), its.at(0));
-    if(its.at(1) == 14 && its.at(2) == 11) value = ret;
+    if(its.at(1) == 14 && its.at(2) == 11) value = ret?1:0;
     return responRpcString(its, value.toString()); // ret?1:0
 }
 
