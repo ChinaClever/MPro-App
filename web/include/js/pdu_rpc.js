0d0b06eebb1ed50e4e0d7157b3e22b4a7a469927
lzy <luozhiyong>
2023-02-23 16:57:46 +0800

    1

--- web/include/js/pdu_rpc.js
+++ web/include/js/pdu_rpc.js
@@ -101,9 +101,9 @@ class JsonRpc {
         if(this.ws.readyState == WebSocket.OPEN){     
             this.ws.send(msg);
         } else {
-            ret = false;
-            this.ws.close();
-            this.ws = this.socket_open(); 
+            // ret = false;
+            // this.ws.close();
+            // this.ws = this.socket_open(); 
         }
         return ret;
     }
