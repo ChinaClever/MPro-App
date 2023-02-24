d4719c46f955aeca69ad3d46562009bfea90d415
lzy <luozhiyong>
2023-02-24 15:43:50 +0800

    增加测试日志功能

--- cores/apps/app_smtp.h
+++ cores/apps/app_smtp.h
@@ -20,8 +20,9 @@ class App_Smtp : public App_Radius
     Q_OBJECT
 public:
     explicit App_Smtp(QObject *parent = nullptr);
-    void smtp_sendMail(const QString &content);
+    void smtp_sendMail(const QString &content, bool ok=false);
     static sSmtpCfg smtpCfg;
+    bool smtp_testMail();
 
 private slots:
     void smtp_run();
