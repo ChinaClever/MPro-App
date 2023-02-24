1861dfc7dc7a39c9da37a0c65a40ab8c744e2e2b
lzy <luozhiyong>
2023-02-24 17:12:40 +0800

    cc

--- cores/commons/cms/commons.cpp
+++ cores/commons/cms/commons.cpp
@@ -75,6 +75,8 @@ bool cm::qrcodeGenerator(const QString& msg)
     QString fn = "/usr/data/clever/cfg/qrcode.png";
     QString cmd = "qrencode -o %1 -s 6 '%2'";
     QString qr = cmd.arg(fn, msg);
+    QString rm = "rm -f " +fn;
+    system(rm.toUtf8().data());
     system(qr.toUtf8().data());
     cout << qr;
     return true;
