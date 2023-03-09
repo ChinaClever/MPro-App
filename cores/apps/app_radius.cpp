9708fcc730882ec1d43657964f63e827d83a60f1
lzy <luozhiyong>
2023-03-09 10:47:11 +0800

    验证Radius功能

--- cores/apps/app_radius.cpp
+++ cores/apps/app_radius.cpp
@@ -18,9 +18,9 @@ int App_Radius::radius_work(const QString &usr, const QString &pwd)
     QString fmd = "radiuscfg %1 %2 %3 %4"; sRadiusCfg *cfg = &radiusCfg;
     QString cmd = fmd.arg(cfg->host).arg(cfg->authPort).arg(cfg->acctPort).arg(cfg->key);
     cm::execute(cmd);  fmd = "radius %1 %2"; cmd = fmd.arg(usr, pwd);
-    int ret = 0; QString res = cm::execute(cmd);
+    int ret = 0; QString res = cm::execute(cmd); //cout << cmd << res;
     if(res.contains("NORES")) ret = -1;
-    else if(res.contains("ERR")) ret = 0;
-    else ret = 1;
+    else if(res.contains("OK")) ret = 1;
+    else ret = 0;
     return ret;
 }
