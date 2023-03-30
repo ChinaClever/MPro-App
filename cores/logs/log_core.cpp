41e8491afb974409f5b079805e36d9b347b1a467
lzy <luozhiyong>
2023-03-30 13:01:32 +0800

    OK

--- cores/logs/log_core.cpp
+++ cores/logs/log_core.cpp
@@ -68,21 +68,21 @@ void Log_Core::append(const sDataItem &it)
     hda.topic = QString::number(it.topic);
     hda.indexes = QString::number(it.id + 1);
     hda.value = QString::number(it.value / cm::decimal(it));
-    mHdaIts << hda; run();
+    mHdaIts << hda; run(); //cout << hda.addr << hda.type << hda.topic << hda.indexes << hda.value;
 }
 
 void Log_Core::log_hda(const sDataItem &it)
 {
-    uint sec = cfg.hdaTime*60*60;
-    //uint sec = cfg.hdaTime*60; //////////////============
+    //uint sec = cfg.hdaTime*60*60;
+    uint sec = cfg.hdaTime*60; //////////////============
     if(!cfg.hdaEn || !sec) return ;
     if(!(mCnt%sec)) append(it);
 }
 
 void Log_Core::log_hdaEle(const sDataItem &it)
 {
-    uint sec = cfg.eleTime*24*60*60;
-    //uint sec = cfg.eleTime*60;  ////////////=============
+    //uint sec = cfg.eleTime*24*60*60;
+    uint sec = cfg.eleTime*60;  ////////////=============
     if(!cfg.hdaEn || !sec) return ;
     if(!(mCnt%sec)) append(it);
 }
