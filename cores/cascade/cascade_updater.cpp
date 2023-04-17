bb92c8c1705a3800e3dbdf49d5e3eeccf7153316
luozhiyong <luozhiyong131@qq.com>
2023-04-17 13:09:21 +0800

    修改升级目录问题

--- cores/cascade/cascade_updater.cpp
+++ cores/cascade/cascade_updater.cpp
@@ -142,11 +142,11 @@ void Cascade_Updater::otaRecvFinishSlot(const sOtaFile &it, bool ok)
 {
     if(ok){
         QString fn = it.path + it.file;
-        QString dir = "/tmp/updater/ota_apps/";
+        QString dir = "/usr/data/ota_apps/";
         QString str = "unzip -o %1 -d " + dir;
         qDebug() << cm::execute(str.arg(fn));
         system("chmod 777 -R /usr/data/clever/");
-        system("chmod 777 -R /tmp/updater/ota_apps/");
+        system("chmod 777 -R /usr/data/ota_apps/");
         QString fmd = "rsync -av --exclude rootfs/  %1 /usr/data/clever/";
         QString cmd = fmd.arg(dir); throwMessage(cmd);
         str = cm::execute(cmd); throwMessage(str);
@@ -189,7 +189,7 @@ void Cascade_Updater::otaReboot()
     system("rm -rf /usr/data/clever/outlet/*");
     system("chmod 777 /usr/data/clever/bin/*");
     system("chmod 777 /usr/data/clever/app/*");
-    system("rm -rf /tmp/updater/ota_apps");
+    system("rm -rf /usr/data/ota_apps");
     system("rm -rf /usr/data/upload/*");
     system("sync"); system("reboot");
 }
