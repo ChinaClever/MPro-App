2399cd965c6ab33014d6092f0b13bf7c9d111174
luozhiyong <luozhiyong131@qq.com>
2023-05-04 15:16:00 +0800

    rest

--- cores/commons/http/http.h
+++ cores/commons/http/http.h
@@ -5,8 +5,8 @@
 class Http
 {
 public:
-    static QPair<bool, QByteArray> post(const QString &url, const QByteArray &body, const int &timeout=1000);
-    static QPair<bool, QByteArray> put(const QString &url, const QByteArray &body, const int &timeout=1000);
+    static QPair<bool, QByteArray> post(const QString &url, const QByteArray &body, const int &timeout=1);
+    static QPair<bool, QByteArray> put(const QString &url, const QByteArray &body, const int &timeout=1);
 };
 
 #endif // HTTP_H
