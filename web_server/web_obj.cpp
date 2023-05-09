1c4a9eb4063c5cbe724e6101915039ba73dbad0b
luozhiyong <luozhiyong131@qq.com>
2023-05-09 13:29:04 +0800

    Update web_obj.cpp

--- web_server/web_obj.cpp
+++ web_server/web_obj.cpp
@@ -29,7 +29,7 @@ QString Web_Obj::createUuid()
 
 bool Web_Obj::checkUuid(const QString &uuid)
 {
-    bool ret = false;  //////////====================
+    bool ret = true;  //////////====================
     if(mUuid.size() && (mUuid == uuid)) ret = true;
     else cout << mUuid << uuid;
     return ret;
